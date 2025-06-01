1f6258ce8ce25d1d8a4393acaaf3a74b6a17da48
julian_nic <julian_nic@hotmail.com>
2025-06-01 17:01:49 -0300

    Agregado layout inicial y vista preliminar para sección de servicios

--- src/app/(tabs)/_layout.jsx
+++ src/app/(tabs)/_layout.jsx
@@ -2,23 +2,50 @@ import { Tabs } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons'
 
 const TabsLayout = () => {
-   
   return (
-   
     <Tabs>
-  <Tabs.Screen 
-    name="index" 
-    options={{ title: "Inicio", tabBarIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} /> }} 
-  />
- 
-  <Tabs.Screen 
-    name="perfil" 
-    options={{ title: "Perfil", tabBarIcon: ({ color, size }) => <Ionicons name="person" color={color} size={size} /> }} 
-  />
-</Tabs>
-
+      <Tabs.Screen 
+        name="index" 
+        options={{ 
+          title: "Inicio", 
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => <Ionicons name="home-outline" color={color} size={size} /> 
+        }} 
+      />
+      <Tabs.Screen 
+        name="buscar" 
+        options={{ 
+          title: "Buscar", 
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => <Ionicons name="search-outline" color={color} size={size} /> 
+        }} 
+      />
+      <Tabs.Screen 
+        name="publicar" 
+        options={{ 
+          title: "Publicar", 
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => <Ionicons name="add-outline" color={color} size={size} /> 
+        }} 
+      />
+      <Tabs.Screen 
+        name="servicios" 
+        options={{ 
+          title: "Servicios", 
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => <Ionicons name="storefront-outline" color={color} size={size} /> 
+        }} 
+      />
+      <Tabs.Screen 
+        name="perfil" 
+        options={{ 
+          title: "Perfil",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => <Ionicons name="person-outline" color={color} size={size} /> 
+        }} 
+      />
+    </Tabs>
   )
 }
 
-export default TabsLayout;
-
+export default TabsLayout
---
+++ src/app/(tabs)/buscar.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Buscar() {
+  return (
+    <View>
+      <Text>Pantalla Buscar</Text>
+    </View>
+  )
+}
